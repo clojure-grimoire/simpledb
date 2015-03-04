15e2f9989ec667178d531066f432eae3eb809a0a
arrdem <me@arrdem.com>
2015-03-03 21:30:44 -0600

    lol @ clojure 1.2

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject simpledb "0.1.4"
+(defproject org.clojure-grimoire/simpledb "0.1.4"
             :description "A simple in-memory database for use with prototypes."
-            :dependencies [[org.clojure/clojure "1.2.1"]]
-            :dev-dependencies [[lein-clojars "0.6.0"]]
+            :dependencies [[org.clojure/clojure "1.6.0"]]
             :main simpledb.core)
