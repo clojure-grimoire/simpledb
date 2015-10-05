4a428e0ca819f3acf616f237814343c64cca76e2
Julian Gamble <mail@juliangamble.com>
2015-10-05 16:00:39 +1100

    Fixes to tests and making it easier for newbies.

--- src/simpledb/core.clj
+++ src/simpledb/core.clj
@@ -58,7 +58,7 @@
 (defn- compute-interval [[i t]]
   {:pre [(#{:seconds :minutes :hours :days} i)
          (number? t)
-         (> 0 t)]}
+         (> t 0)]}
   (case i
     (:seconds) (long t)
     (:minutes) (recur [:seconds (* t 60)])
@@ -72,7 +72,7 @@
         _        (assert timer)
         interval (:interval cfg)
         interval (compute-interval interval)]
-    (assert (not (.isShutDown timer)) "Timer is shut down, db connection is closed!")
+    (assert (not (.isShutdown timer)) "Timer is shut down, db connection is closed!")
     (. timer (scheduleAtFixedRate (partial flush! cfg) interval interval (. TimeUnit SECONDS)))))
 
 (defn stop! [cfg]
