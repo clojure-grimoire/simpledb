8d7051c345cc2d4345770a342939c854bc4401d7
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-11-12 21:54:59 -0600

    Add URL, license

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject org.clojure-grimoire/simpledb "0.1.7"
   :description "A simple in-memory database for use with prototypes."
+  :url "http://github.com/clojure-grimoire/simpledb"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
