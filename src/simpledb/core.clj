088e540debbe7d047bda45e4e37f099fdd3a27d6
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-12-23 22:21:10 -0600

    Refactor to use Clojure watches
    This patch replaces the existing polling implementation of SimpleDB with
    Clojure's reference watcher functionality. When the backing atom is
    swap!ed, this triggers the watcher fn which aquires a lock and flushes
    the database. This means that the database will be flushed eagerly on
    write rather than somewhat continuously flushed on a real time clock
    tick.
    
    While undoubtably slower, this is also saner so deal.

--- src/simpledb/core.clj
+++ src/simpledb/core.clj
@@ -1,7 +1,5 @@
 (ns simpledb.core
-  (:refer-clojure :exclude [get get-in])
-  (:import java.util.concurrent.TimeUnit
-           java.util.concurrent.Executors))
+  (:refer-clojure :exclude [get get-in]))
 
 (defn put! [cfg k v]
   (let [*db* (:db cfg)]
@@ -33,13 +31,16 @@
 (defn flush! [cfg]
   (let [*db*   (:db cfg)
         log-fn (:log-fn cfg)
+        lock   (::lock cfg)
         cur    @*db*]
-    (log-fn (str "SimpleDB: Persisting " (count cur) " keys."))
-    (spit (:file cfg) (pr-str cur))))
+    (locking lock
+      (log-fn (str "SimpleDB: Persisting " (count cur) " keys."))
+      (spit (:file cfg) (pr-str cur)))))
 
 (defn read! [cfg]
   (let [*db*    (:db cfg)
         log-fn  (:log-fn cfg)
+        lock    (::lock cfg)
         content (try
                   (read-string (slurp (:file cfg)))
                   (catch Exception e
@@ -56,41 +57,19 @@
     (reset! db {})
     (flush! cfg)))
 
-(defn- compute-interval [[i t]]
-  {:pre [(#{:seconds :minutes :hours :days} i)
-         (number? t)
-         (> t 0)]}
-  (case i
-    (:seconds) (long t)
-    (:minutes) (recur [:seconds (* t 60)])
-    (:hours)   (recur [:minutes (* 60 t)])
-    (:days)    (recur [:hours (* 24 t)])))
-
 ;; Startup/shutdown operations
 ;;------------------------------------------------------------------------------
 (defn start! [cfg]
-  (let [timer    (:timer cfg)
-        _        (assert timer)
-        interval (:interval cfg)
-        interval (compute-interval interval)]
-    (assert (not (.isShutdown timer)) "Timer is shut down, db connection is closed!")
-    (. timer (scheduleAtFixedRate (partial flush! cfg) interval interval (. TimeUnit SECONDS)))))
+  (add-watch (:db cfg) ::watcher (fn [& _] (flush! cfg))))
 
 (defn stop! [cfg]
-  (let [timer (:timer cfg)]
-    (flush! cfg)
-    (. timer (shutdown))
-    (dissoc cfg :timer :db)))
+  (remove-watch (:db cfg) ::watcher))
 
 (defn init!
   "Initializes a database.
 
   Accepts an optional map of settings
    - :file is the file to & from which the db will be written
-
-   - :interval is the rate as a keyword, num pair w here the kw is one
-     of #{:minutes :seconds :hours} and the number is how long in those
-     to wait between writes.
 
    - :log-fn is a function of several arguments to be called when logging
      messages are generated, being a read, write start or shutdown.
@@ -103,23 +82,16 @@
   {:arglists '([] [{:keys [db file interval log-fn no-slurp]}])}
   [& [{:keys [file interval log-fn no-slurp db]
        :or   {file     "sdb.db"
-              interval [:minutes 5]
               log-fn   (fn [& _] nil)
               no-slurp false}}]]
-  (assert (vector? interval))
-  (assert (#{:minutes :seconds :hours} (first interval)))
-  (assert (number? (second interval)))
   (assert (fn? log-fn))
   
   (let [user-db  db
-        timer    (. Executors newScheduledThreadPool 1)
-
         ;; Final configuration
         cfg      {:db       (or user-db (atom {}))
                   :file     file
-                  :timer    timer
-                  :interval interval
-                  :log-fn   log-fn}]
+                  :log-fn   log-fn
+                  ::lock    (Object.)}]
     (when-not no-slurp
       (read! cfg))
     (start! cfg)
