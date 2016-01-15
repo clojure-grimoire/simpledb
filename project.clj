ace035892733bdd803ef7a5adcbb6dc5152b9158
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-01-15 17:20:50 -0600

    Vist 0.2.1 upon the world

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure-grimoire/simpledb "0.2.0"
+(defproject org.clojure-grimoire/simpledb "0.2.1"
   :description "A simple in-memory database for use with prototypes."
   :url "http://github.com/clojure-grimoire/simpledb"
   :license {:name "Eclipse Public License"
